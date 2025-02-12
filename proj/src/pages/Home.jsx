2e69c6947c84dd4b524118b95f30bfcaa35e5609
hmz akt <hmzakt11@gmail.com>
2025-02-12 19:12:34 +0530

    Minor tweaks for not registered users ui

--- proj/src/pages/Home.jsx
+++ proj/src/pages/Home.jsx
@@ -4,8 +4,7 @@ import appwriteService from "../appwrite/config";
 import { useSelector } from 'react-redux';
 import { Container, Postcard } from "../components";
 
-
-function AltHome() {
+function Home() {
   const authStatus = useSelector((state) => state.auth.status);
   const navigate = useNavigate();
 
@@ -14,13 +13,13 @@ function AltHome() {
       name: 'Create',
       slug: '/add-post',
       active: authStatus,
-      image : "/svgs/create.svg"
+      image: "/svgs/create.svg"
     },
     {
       name: 'All posts',
       slug: '/all-posts',
-      active: authStatus, 
-      image : "/svgs/allposts.svg"// Only show when authenticated
+      active: authStatus,
+      image: "/svgs/allposts.svg"
     }
   ];
 
@@ -46,22 +45,26 @@ function AltHome() {
   if (posts.length === 0) {
     return (
       <div className='flex flex-row flex-wrap'>
-      <div className="w-full lg:w-[80%] py-8 min-h-80">
-        <Container>
-          <div className="text-2xl mb-6 pl-8">
-            Loading...
-          </div>
-        </Container>
-      </div>
-      
-      <div className='w-[20%] py-8 pt-20 justify-center items-start hidden lg:block p'>
-        <ul className>
-          {navItems.map((item) =>
-            item.active ? (
+        <div className="w-full lg:w-[80%] py-8 min-h-80">
+          <Container>
+            <div className="text-2xl mb-6 pl-8">
+              Loading...
+            </div>
+          </Container>
+        </div>
+
+        <div className='w-[20%] py-8 pt-20 justify-center items-start hidden lg:block p'>
+          <ul>
+            {navItems.map((item) => (
               <li key={item.name}>
                 <button
-                  bgColor="bg-transparent"
-                  onClick={() => navigate(item.slug)}
+                  onClick={() => {
+                    if (item.active) {
+                      navigate(item.slug);
+                    } else {
+                      alert("You need to login to begin");
+                    }
+                  }}
                   className="px-4 ml-20 mb-10 py-2 h-40 w-40 text-black"
                 >
                   <div>
@@ -72,70 +75,68 @@ function AltHome() {
                   </div>
                 </button>
               </li>
-            ) : (
-              alert("You need to login to begin")
-            )
-          )}
-        </ul>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
-      );
+    );
   }
 
   return (
     <div className='flex flex-row flex-wrap'>
-  <div className="w-full lg:w-[80%] py-8 min-h-80">
-    <Container>
-      <div className="text-2xl mb-6 pl-8">
-        Recent Posts...
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
-        {posts.slice(0, visiblePosts).map((post) => (
-          <div key={post.$id} className="p-2">
-            <Postcard {...post} />
+      <div className="w-full lg:w-[80%] py-8 min-h-80">
+        <Container>
+          <div className="text-2xl mb-6 pl-8">
+            Recent Posts...
           </div>
-        ))}
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+            {posts.slice(0, visiblePosts).map((post) => (
+              <div key={post.$id} className="p-2">
+                <Postcard {...post} />
+              </div>
+            ))}
+          </div>
+          {visiblePosts < posts.length && (
+            <div className="flex justify-center mt-8">
+              <button
+                onClick={loadMorePosts}
+                className="bg-black text-white px-4 py-2 rounded-3xl hover:bg-grey transition-colors"
+              >
+                Load More
+              </button>
+            </div>
+          )}
+        </Container>
       </div>
-      {visiblePosts < posts.length && (
-        <div className="flex justify-center mt-8">
-          <button
-            onClick={loadMorePosts}
-            className="bg-black text-white px-4 py-2 rounded-3xl  hover:bg-grey transition-colors"
-          >
-            Load More
-          </button>
-        </div>
-      )}
-    </Container>
-  </div>
-  
-  {/* Navigation buttons - only visible on large screens */}
-  <div className='w-[20%] py-8 pt-20 justify-center items-start hidden lg:block p'>
-    <ul className>
-      {navItems.map((item) =>
-        item.active ? (
-          <li key={item.name}>
-            <button
-              bgColor="bg-transparent"
-              onClick={() => navigate(item.slug)}
-              className="px-4 ml-20 mb-10 py-2 h-40 w-40 text-black"
-            >
-              <div>
-                <img src={item.image} alt={item.name} />
-              </div>
-              <div>
-                {item.name}
-              </div>
-            </button>
-          </li>
-        ) : (
-          alert("You need to login to begin")
-        )
-      )}
-    </ul>
-  </div>
-</div>
+
+      {/* Navigation buttons - only visible on large screens */}
+      <div className='w-[20%] py-8 pt-20 justify-center items-start hidden lg:block p'>
+        <ul>
+          {navItems.map((item) => (
+            <li key={item.name}>
+              <button
+                onClick={() => {
+                  if (item.active) {
+                    navigate(item.slug);
+                  } else {
+                    alert("You need to login to begin");
+                  }
+                }}
+                className="px-4 ml-20 mb-10 py-2 h-40 w-40 text-black"
+              >
+                <div>
+                  <img src={item.image} alt={item.name} />
+                </div>
+                <div>
+                  {item.name}
+                </div>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
-export default AltHome;
+export default Home;
