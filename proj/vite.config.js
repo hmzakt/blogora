d7e16e77f007ad88f06838fc0bf780ee8d5c7c49
hmz akt <hmzakt11@gmail.com>
2025-02-17 00:27:30 +0530

    some minor tweaks

--- proj/vite.config.js
+++ proj/vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/', // Ensures correct asset paths for deployment
+  server: {
+    historyApiFallback: true,
+  },
 })
